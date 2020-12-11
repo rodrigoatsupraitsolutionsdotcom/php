e6233b1151b5c507d2fbcb80d1269acc1c39a341
rodrigoatsupraitsolutionsdotcom <rodrigo@supraitsolutions.com>
2020-12-10 21:21:47 -0300

    Create boolean.php

---
+++ boolean.php
@@ -0,0 +1,26 @@
+<?php
+
+/* FALSE */
+# $a = false;
+# $a = 0;
+# $a = 0.0;
+# $a = "";
+# $a = '';
+# $a = "0";
+# $a = NULL;
+# $a = [];
+# $a = array();
+
+$a = '0';
+
+
+if (empty($a)) { 
+
+	echo 'EH VAZIA';
+
+} else {
+
+	echo 'NAO EH VAZIA';
+
+}
+
