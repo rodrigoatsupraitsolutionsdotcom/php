b5a32fb1ca385d1dfc301893d21c1a169fea8f49
rodrigoatsupraitsolutionsdotcom <rodrigo@supraitsolutions.com>
2020-12-17 20:22:49 -0300

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$person[0] = "Edison";
+$person[1] = "Wankel";
+$person[2] = "Crapper";
+$person[3] = null;
+
+$creator['Light bulb'] = "Edison";
+$creator['Rotary Engine'] = "Wankel";
+$creator['Toilet'] = "Crapper";
+
+
+echo $person[2];
+echo '<br>';
+echo $creator['Light bulb'];
