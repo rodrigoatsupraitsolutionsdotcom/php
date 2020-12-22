f3ed8504c3f09fd9ef4e0948d5331a59d14a76e4
rodrigoatsupraitsolutionsdotcom <rodrigo@supraitsolutions.com>
2020-12-22 20:14:48 -0300

    Create variaveis.php

---
+++ variaveis.php
@@ -0,0 +1,16 @@
+<?php
+
+
+	$what = "Fred";
+	$what = 35;
+	$what = array("Fred", 35, "Wilma");
+	
+	$day = 60 * 60 * 24;
+	//echo "There are {$day} seconds in a day.";
+	
+	
+	if ($day === NULL) {
+		echo "Yes!";
+	} else {
+		echo "No!";
+	}
