6f0e375f79d43085d85d3f0b3accc61ccbf4a8b0
Rodrigo Nakahodo <nakahodo@blueservice.com.br>
2021-01-12 20:48:54 -0300

    Create operadores.php

---
+++ operadores.php
@@ -0,0 +1,50 @@
+<?php
+/*
+class Pessoa {}
+
+$rodrigo = new Pessoa();
+
+$pessoas = ["rodrigo", "Bira", ...];
+*/
+/*
+$i = 1;
+
+echo ++$i;
+echo "<br>";
+echo ++$i;
+echo "<br>";
+echo ++$i;
+
+$b = false;
+echo !$b; // $b = true
+*/
+
+$a = 1;
+$b = "1";
+
+if($a == $b){
+	echo "true";echo "<br>";
+}else{
+	echo "false";echo "<br>";
+}
+
+if($a === $b){
+	echo "true";echo "<br>";
+}else{
+	echo "false";echo "<br>";
+}
+
+if ($a != $b) {
+	echo "true";echo "<br>";
+}else{
+	echo "false";echo "<br>";
+}
+
+if ($a !== $b) {
+	echo "true";echo "<br>";
+}else{
+	echo "false";echo "<br>";
+}
+
+
+
