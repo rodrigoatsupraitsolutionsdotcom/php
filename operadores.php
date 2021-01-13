c7864fd6890895b26b9b029b2cfc87bd7d591de3
Rodrigo Nakahodo <nakahodo@blueservice.com.br>
2021-01-12 21:34:47 -0300

    Update operadores.php

--- operadores.php
+++ operadores.php
@@ -18,9 +18,9 @@ echo ++$i;
 $b = false;
 echo !$b; // $b = true
 */
-
+/*
 $a = 1;
-$b = "1";
+$b = 1;
 
 if($a == $b){
 	echo "true";echo "<br>";
@@ -45,6 +45,54 @@ if ($a !== $b) {
 }else{
 	echo "false";echo "<br>";
 }
+*/
+/*
+$a = true;
+$b = true;
+
+if ($a xor $b) {
+	echo "true";echo "<br>";
+}else{
+	echo "false";echo "<br>";
+}
+*/
+
+//echo ($a ? $b : $c);
+
+
+#echo 3 . 2.74;
+#echo "1 casa" + "2 cachorros";
+
+/*
+$x = "spaz";
+echo ++$x;
+*/
+
+/*
+$a = 4;
+$b = 4;
+
+echo $a <=> $b;
+
+
+echo $a ?? $b ?? $c ?? $d;
+
+
+if($a && $b){}
+
+if($a || $b){}
+*/
+
+echo !false;
+
+
+
+
+
+
+
+
+
 
 
 
