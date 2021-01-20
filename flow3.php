d32608be6633263bb26cafea1c8aff805c66ccd3
Rodrigo Nakahodo <nakahodo@blueservice.com.br>
2021-01-20 20:43:34 -0300

    Update flow3.php

--- flow3.php
+++ flow3.php
@@ -15,3 +15,27 @@ for ($i = 0, $j = 1; $i <= 10; $i++, $j++) {
 
 echo $total;
 */
+/*
+$meusNumeros = [1,2,3,4,5,6,7,8,9,10];
+
+foreach ($meusNumeros as $numero) {
+
+	echo $numero . '<br>';
+}
+
+echo $numero;
+*/
+
+/*
+$animais = array();
+$animais['A'] = 'Gustavo';
+$animais['B'] = 'Benji';
+
+foreach ($animais as $x => $y) {
+	
+	echo 'A chave é: '. $x . ' e o value é ' . $y . ' <BR>';
+}
+
+echo 'A chave é: '. $x . ' e o value é ' . $y . ' <BR>';
+
+*/
