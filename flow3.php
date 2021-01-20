3377b128aa30e3dad7c6db2cd0a11b97333669cf
Rodrigo Nakahodo <nakahodo@blueservice.com.br>
2021-01-20 20:34:24 -0300

    Create flow3.php

---
+++ flow3.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+	for ($counter = 0; $counter < 10; $counter++) {
+		echo "Counter is $counter <br/>";
+	}
+*/
+
+/*
+$total = 0;
+
+for ($i = 0, $j = 1; $i <= 10; $i++, $j++) {
+	$total += $j;
+}
+
+echo $total;
+*/
