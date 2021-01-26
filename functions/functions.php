93c4ed8bb10e277f5b399fe2f1a36ea07bd8907b
Rodrigo Nakahodo <nakahodo@blueservice.com.br>
2021-01-26 20:48:32 -0300

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -16,9 +16,100 @@ $result = unlink("functions.txt");
 $result = unlink("functions.txt") or die("Operation failed!");
 
 */
-
+/*
 function [&] function_name([parameter[, ...]])
 {
  statement list
 }
+*/
+/*
+function column()
+{
+ echo '</td><td>';
+}
 
+?>
+
+
+<?php function column()
+{ ?>
+ </td><td>
+<?php } ?>
+*/
+
+/*
+function strcat($left, $right)
+{
+ $combinedString = $left . ' ' . $right;
+
+ return $combinedString;
+}
+
+$nome_completo = strcat('Rodrigo', 'Nakahodo'); // Rodrigo Nakahodo
+
+
+function strcat($left, $right)
+{
+ return $left . ' ' . $right;
+}
+
+
+*/
+
+
+function pulaLinha() 
+{
+	print '<BR>';
+}
+
+function printarPalavrasJuntas($a, $b)
+{
+	print $a . ' - ' . $b;
+}
+
+#************************************************
+/*
+$x = "João ";
+$y = "Guilherme ";
+$z = "Rodrigo ";
+$w = "Nakahodo ";
+
+printarPalavrasJuntas($x, $y);
+pulaLinha();
+pulaLinha();
+printarPalavrasJuntas($z, $w);
+pulaLinha();
+pulaLinha();
+printarPalavrasJuntas('Rodrigo ', ' Nakahodo');
+pulaLinha();
+pulaLinha();
+pulaLinha();
+
+echo ucfirst('rodrigo');
+
+pulaLinha();
+pulaLinha();
+
+
+echo strtoupper(str_repeat('bira ', 10));
+*/
+/*
+function nosso_str_repeat($palavra, $quantas_vezes)
+{
+	
+	$palavra_final = '';
+
+	$i = 0;
+	
+	while($quantas_vezes > $i){
+		
+		$palavra_final = $palavra_final . $palavra;
+
+		$i++;
+	}
+	
+	return $palavra_final;
+}
+
+echo nosso_str_repeat('Pablo', 100);
+*/
